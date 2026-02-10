e191e9aca1b9a6b70cb9dc49878aa8641b312289
tecrcopentgit <abdulhaadhi015@gmail.com>
2026-02-10 12:36:14 +0530

    users

--- src/NodeSection/userM_server.js
+++ src/NodeSection/userM_server.js
@@ -145,7 +145,7 @@ alert(err);
 
 net.get("/users", async (req, res) => {
   try {
-    const result = await q.query("SELECT user_name FROM userMedTable");
+    const result = await q.query("SELECT * FROM userMedTable");
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: "Error fetching users" });
